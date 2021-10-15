9cad299001cb85122651794b246379c1cb8cf671
eslam.gomaa <eslam.sa3dany2016@gmail.com>
2021-10-15 21:46:15 +0000

    Update the example

--- Example-2.py
+++ Example-2.py
@@ -16,17 +16,17 @@ for host in hosts['hosts']:
     connection.execute(cmd='enable\n' + enable_pwd)
     # connection.execute(cmd='enable\n')
 
-    connection.print("Testing using conditions...", level='info')
     check_version = connection.execute(cmd="sh version", search='Cisco IOS Software, IOSv Software', exit_on_fail=False)
-    if check_version['search_found?']:
-        connection.print("This device is a Cisco Switch", level='warn')
+    if not check_version['search_found?']:
+        connection.print("Based on the output of the last command --> This device is a Cisco Switch", level='warn')
 
     connection.print('Let\'s do some more testing ... ^_^', level='info')
     connection.print('Will run a command based on the "exit_code" of another command', level='alert')
     if connection.execute(cmd="show ip int br", print_json=True, ask_for_confirmation=False)['exit_code'] == 0:
-        connection.backup_config(comment='Taking backup before running important command')
-        connection.print('The next command will ask for confirmation before execution', level='info')
-        connection.execute_from_file(file='conf_file.txt', print_json=False, ask_for_confirmation=False)
+        connection.backup_config(comment='Taking backup before running important commands')
+        connection.print('Now will execute commands from a file', level='info')
+        connection.print('Each command will be executed separately & will abort an error was encountered', level='warn')
+        connection.execute_from_file(file='conf_file.txt', print_json=False, ask_for_confirmation=True)
         connection.backup_config(comment='Taking another backup')
 
 
